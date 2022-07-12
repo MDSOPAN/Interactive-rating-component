49f8c2921afbc0b2b0c7109eae15f057054adaf0
MDSOPAN <77196081+MDSOPAN@users.noreply.github.com>
2022-07-13 00:15:11 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const btn = document.querySelectorAll(".ratenum button");
+let ra = sessionStorage.getItem("rating") || 0;
+(() => {
+  if (ra != 0) {
+    btn[ra - 1].classList.add("selected");
+  }
+})();
+console.log(btn);
+btn.forEach((el) => {
+  el.addEventListener("click", (ele) => {
+    ra = ele.target.innerHTML;
+    sessionStorage.setItem("rating", `${ra}`);
+    btn.forEach((el) => {
+      el.classList.remove("selected");
+    });
+    ele.target.classList.add("selected");
+  });
+});
+const subbtn = document.querySelector(".subbtn");
+subbtn.addEventListener("click", () => {
+  if (ra == 0) {
+    window.alert("Please Select a Rating");
+  } else {
+    window.location.href = "/submit.html";
+  }
+});
---
+++ sub.js
@@ -0,0 +1,3 @@
+document.querySelector(
+  ".thtext"
+).innerHTML = `You selected ${sessionStorage.getItem("rating")} out of 5`;
